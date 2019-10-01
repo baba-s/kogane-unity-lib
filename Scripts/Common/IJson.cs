ac89726b9b7095fe873b89689699131cadcafa42
baba_s <shotababa1206@gmail.com>
2019-10-01 13:33:02 +0900

    add json interface

---
+++ Scripts/Common/IJson.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace KoganeUnityLib
+{
+	public interface IJson
+	{
+	}
+
+	public static class IJsonExt
+	{
+		public static string ToReadableJson( this IJson self )
+		{
+			return JsonUtility.ToJson( self, true );
+		}
+
+		public static string ToJson( this IJson self )
+		{
+			return JsonUtility.ToJson( self );
+		}
+	}
+}
