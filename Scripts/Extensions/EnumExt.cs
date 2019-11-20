7dde6ff7b08e2eadf79e51e5ae7f2cb836960a89
baba_s <shotababa1206@gmail.com>
2019-11-20 13:35:47 +0900

    add

--- Scripts/Extensions/EnumExt.cs
+++ Scripts/Extensions/EnumExt.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 namespace KoganeUnityLib
 {
@@ -21,12 +22,49 @@ namespace KoganeUnityLib
 		}
 
 		/// <summary>
+		/// 前の値に進みます。最小値を超える場合は最大値に戻ります
+		/// </summary>
+		public static T PrevLoop<T>( this T self )
+		{
+			var intValue = Convert.ToInt32( self );
+			var length = EnumUtils.GetLength<T>();
+			var nextValue = ( intValue - 1 + length ) % length;
+			var enumValue = EnumUtils.ToObject<T>( nextValue );
+
+			return enumValue;
+		}
+
+		/// <summary>
 		/// 次の値に進みます。最大値を超える場合は初期値に戻ります
 		/// </summary>
 		public static T NextLoop<T>( this T self )
 		{
 			var intValue = Convert.ToInt32( self );
 			var nextValue = ( intValue + 1 ) % EnumUtils.GetLength<T>();
+			var enumValue = EnumUtils.ToObject<T>( nextValue );
+
+			return enumValue;
+		}
+
+		/// <summary>
+		/// 前の値に進みます。最小値を超える場合は止まります
+		/// </summary>
+		public static T Prev<T>( this T self )
+		{
+			var intValue = Convert.ToInt32( self );
+			var nextValue = Mathf.Max( 0, intValue - 1 );
+			var enumValue = EnumUtils.ToObject<T>( nextValue );
+
+			return enumValue;
+		}
+
+		/// <summary>
+		/// 次の値に進みます。最大値を超える場合は止まります
+		/// </summary>
+		public static T Next<T>( this T self )
+		{
+			var intValue = Convert.ToInt32( self );
+			var nextValue = Mathf.Min( EnumUtils.GetLength<T>(), intValue + 1 );
 			var enumValue = EnumUtils.ToObject<T>( nextValue );
 
 			return enumValue;
